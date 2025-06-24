435ffa91db1c3f1baa8fe144bac32cd98823d0ec
jlddorf <jakob.deutschendorf@gmail.com>
2025-06-24 18:53:00 +0200

    start theme updates

--- ui/player_container.gd
+++ ui/player_container.gd
@@ -3,8 +3,8 @@ class_name PlayerContainer
 
 const GlobalValues = preload("res://scripts/globalValues.gd")
 
-@onready var placed_amount_bar: AmountProgress = $VBoxContainer/Panel/VBoxContainer/MarginContainer/PlacedAmountBar
-@onready var item_display: ItemDisplay = $VBoxContainer/MarginContainer/ItemDisplay
+@onready var placed_amount_bar: AmountProgress = $VBoxContainer/Panel/VBoxContainer/HBoxContainer/MarginContainer2/PlacedAmountBar
+@onready var item_display: ItemDisplay = $VBoxContainer/Panel/VBoxContainer/HBoxContainer/MarginContainer/ItemDisplay
 
 func changeValue(target: float):
 	placed_amount_bar.changeValue(target)
