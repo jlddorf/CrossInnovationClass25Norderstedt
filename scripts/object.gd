b47bc0b684b95d4357f8589dfbbc2062d093b1f3
jlddorf <jakob.deutschendorf@gmail.com>
2025-06-27 21:56:43 +0200

    add disappear tween for objects

--- scripts/object.gd
+++ scripts/object.gd
@@ -1,7 +1,13 @@
 extends MeshInstance3D
+class_name MapObject
 
 func _ready() -> void:
 	var tween : Tween = self.create_tween()
 	self.scale = Vector3(0.5, 0.5, 0.5)
 	tween.tween_property(self, "scale", Vector3(1.3, 1.3, 1.3), 0.5)
 	tween.tween_property(self, "scale", Vector3(1.0, 1.0, 1.0), 0.1)
+
+func remove() -> void:
+	var tween : Tween = self.create_tween()
+	tween.set_ease(Tween.EASE_IN).tween_property(self, "scale", Vector3(0.1, 0.1, 0.1), 0.2)
+	tween.tween_callback(self.queue_free)
--- scripts/object_manager.gd
+++ scripts/object_manager.gd
@@ -3,6 +3,8 @@ extends Node
 const CustomTypes = preload("res://scripts/custom_types.gd")
 const OBJECT : PackedScene = preload("res://scenes/object.tscn")
 
+@export var target_mesh: MeshInstance3D
+
 var placed_items: Dictionary[CustomTypes.Item, Array]
 var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 
@@ -12,15 +14,18 @@ func _on_player_manager_update_player_item(_player_id: int, item: int, new_amoun
 	# If item difference is greater than 0, place items to balance it out
 	while item_difference > 0:
 		var node : Node3D = OBJECT.instantiate()
+		node.position = get_random_coordinate_on_mesh()
 		self.add_child(node)
-		node.position = Vector3(rng.randf_range(-2.0, 2.0), 0.25, rng.randf_range(-2.0, 2.0))
 		item_difference -= 1
 		placed_items[item].append(node)
 	# If item difference is less than 0, remove items to balance it out
 	while item_difference < 0:
-		var last_item: Node3D = itemList.pop_back() as Node3D
-		last_item.queue_free()
+		var last_item: MapObject = itemList.pop_back() as MapObject
+		last_item.remove()
 		item_difference += 1
 		
-		
-		
+func get_random_coordinate_on_mesh() -> Vector3:
+	var size : Vector3 = target_mesh.mesh.get_aabb().size.abs()
+	var halfX : float = size.x / 2
+	var halfZ : float = size.z / 2
+	return Vector3(rng.randf_range(-halfX, halfX), 0.25, rng.randf_range(-halfZ, halfZ))
