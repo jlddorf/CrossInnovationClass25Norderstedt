62e91903e6295d427ba139478b5cdb76a8b1bc28
jlddorf <jakob.deutschendorf@gmail.com>
2025-06-27 12:59:14 +0200

    start experimenting with object placing

---
+++ scripts/object.gd
@@ -0,0 +1,7 @@
+extends MeshInstance3D
+
+func _ready() -> void:
+	var tween = self.create_tween()
+	self.scale = Vector3(0.5, 0.5, 0.5)
+	tween.tween_property(self, "scale", Vector3(1.3, 1.3, 1.3), 0.5)
+	tween.tween_property(self, "scale", Vector3(1.0, 1.0, 1.0), 0.1)
