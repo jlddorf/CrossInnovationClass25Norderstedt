6a411bcf62311b8c3f2b19bf2d48d58f3555297d
jlddorf <jakob.deutschendorf@gmail.com>
2025-07-05 11:42:50 +0200

    use correct parameter for achievement movement

--- scenes/achievement_rect.gd
+++ scenes/achievement_rect.gd
@@ -2,8 +2,9 @@ extends TextureRect
 class_name AchievementTexture
 
 var achievementTween: Tween
-	
+
 func mark_granted() -> void:
 	achievementTween = self.create_tween().set_loops(0)
 	var viewportCenter : Vector2 = get_viewport_rect().get_center()
-	achievementTween.tween_property(self, "position", viewportCenter, 5)
+	var distanceToMove: Vector2 = viewportCenter - global_position
+	achievementTween.tween_property(self, "position", distanceToMove, 5)
