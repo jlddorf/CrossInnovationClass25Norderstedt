8a9f6853ae18307d2ad89d0fbf459ee378bc1d1a
jlddorf <jakob.deutschendorf@gmail.com>
2025-07-07 19:33:06 +0200

    fix item limit evaluation

--- scripts/player_manager.gd
+++ scripts/player_manager.gd
@@ -8,9 +8,10 @@ signal update_player_item(player_id: int, item: CustomTypes.Item, currentAmount:
 static var selected_items : Dictionary[int, CustomTypes.Item]
 
 # Dictionary of every item and it's respective placement amount. If this includes NONE, something has gone very wrong 
-static var item_progress: Dictionary[CustomTypes.Item, int]
+static var item_amounts: Dictionary[CustomTypes.Item, int]
 
-static var item_amount : int = 0
+# Amount of placed items
+static var item_count : int = 0
 
 func _ready() -> void:
 	var socket_manager : Array[Node] = get_tree().get_nodes_in_group("SocketManager")
@@ -21,16 +22,17 @@ func _ready() -> void:
 		
 func emit_item_update(player_id: int, change: int) -> void:
 	var current_item : CustomTypes.Item = selected_items.get(player_id, CustomTypes.Item.NONE)
-	var current_amount : int = item_amount
-	update_player_item.emit(player_id, current_item, current_amount, change)
+	update_player_item.emit(player_id, current_item, item_count, change)
 		
 func change_selected_amount_of_player(player_id: int, change: int) -> void:
 	if player_id > 0 && player_id <= CustomTypes.PLAYER_COUNT:
 		var player_item : CustomTypes.Item = selected_items.get_or_add(player_id, CustomTypes.Item.NONE)
 		if (player_item != CustomTypes.Item.NONE):
-			item_progress[player_item] = clamp(item_amount + change, 0, CustomTypes.MAX_AMOUNT_OF_ONE_ITEM)
-			change = clamp(change, -item_amount, CustomTypes.MAX_AMOUNT_OF_ONE_ITEM - item_amount)
-			item_amount += change
+			# Cap change between available items of type and items left
+			change = clamp(change, -item_amounts.get_or_add(player_item, 0), CustomTypes.MAX_AMOUNT_OF_ONE_ITEM - item_count)
+			item_count += change
+			# Clamp item progress of selected item between 0 and max item amount
+			item_amounts[player_item] = clamp(item_amounts[player_item] + change, 0, CustomTypes.MAX_AMOUNT_OF_ONE_ITEM)
 			emit_item_update(player_id, change)
 
 func change_item_of_player(player_id: int, item: CustomTypes.Item) -> void:
