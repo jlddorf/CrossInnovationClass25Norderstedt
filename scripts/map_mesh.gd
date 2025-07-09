e6ee593e300f699794d6876f426f2835ce2a9695
jlddorf <jakob.deutschendorf@gmail.com>
2025-07-09 11:16:36 +0200

    add object placement (only nature rn)

--- scripts/map_mesh.gd
+++ scripts/map_mesh.gd
@@ -1,1 +1,8 @@
 extends MeshInstance3D
+
+func _ready() -> void:
+	var map_mesh : PlaneMesh = mesh
+	var material : StandardMaterial3D = map_mesh.material
+	var texture_aspect : float = material.albedo_texture.get_size().aspect()
+	var mesh_size : Vector2 = map_mesh.size
+	mesh.size.x = mesh_size.y * texture_aspect
--- scripts/object_manager.gd
+++ scripts/object_manager.gd
@@ -8,6 +8,20 @@ const OBJECT : PackedScene = preload("res://scenes/object.tscn")
 var placed_items: Dictionary[CustomTypes.Item, Array]
 var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 
+var allowed_positions: Dictionary[CustomTypes.Item, PackedVector2Array]
+var world_distance_per_pixel: Vector2
+var upper_left_corner : Vector2
+var target_mesh_size: Vector2
+
+func _ready() -> void:
+	allowed_positions[CustomTypes.Item.NATURE] = Constants.PLACEMENT_POINTS_NATURE
+	var placement_mesh: PlaneMesh = target_mesh.mesh
+	var target_mesh_material: StandardMaterial3D = placement_mesh.material
+	target_mesh_size = placement_mesh.size
+	world_distance_per_pixel = target_mesh_size / target_mesh_material.albedo_texture.get_size()
+	upper_left_corner = Vector2(target_mesh.global_position.x, target_mesh.global_position.y) - placement_mesh.size / 2
+	
+
 func _on_player_manager_update_player_item(_player_id: int, item: CustomTypes.Item, _new_amount: int, changed: int) -> void:
 	var itemList: Array = placed_items.get_or_add(item, Array()) as Array
 	#var item_difference: int = new_amount - itemList.size()
@@ -15,7 +29,7 @@ func _on_player_manager_update_player_item(_player_id: int, item: CustomTypes.It
 	while changed > 0:
 		var node : MapObject = OBJECT.instantiate()
 		node.item = item
-		node.position = get_random_coordinate_on_mesh()
+		node.position = get_random_coordinate_on_mesh(item)
 		self.add_child(node)
 		changed -= 1
 		placed_items[item].append(node)
@@ -25,10 +39,10 @@ func _on_player_manager_update_player_item(_player_id: int, item: CustomTypes.It
 		last_item.remove()
 		changed += 1
 		
-func get_random_coordinate_on_mesh() -> Vector3:
-	var viewport: Window = get_viewport()
-	var viewport_size : Vector2 = viewport.size / 2
-	var size : Vector3 = target_mesh.mesh.get_aabb().size.abs()
-	var halfX : float = clamp(size.x / 2, -viewport_size.x, viewport_size.x)
-	var halfZ : float = clamp(size.z / 2, -viewport_size.y, viewport_size.y)
-	return Vector3(rng.randf_range(-halfX, halfX), 0.25, rng.randf_range(-halfZ, halfZ))
+func get_random_coordinate_on_mesh(item: CustomTypes.Item) -> Vector3:
+	var item_array : PackedVector2Array = allowed_positions[item]
+	var random_spot: Vector2 = item_array[randi() % item_array.size()]
+	var spot_on_map: Vector2 = upper_left_corner + random_spot * world_distance_per_pixel
+	# Set height dependent on height on image to avoid wrong drawing order
+	var height : float = 0.05 * (spot_on_map.y / target_mesh_size.y)
+	return Vector3(spot_on_map.x, 0.25 + height, spot_on_map.y)
